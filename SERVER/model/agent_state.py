f4985f409a78eea0e5a1ff6bb0aac30415dfd227
sagarsinha1803 <sagarsinha1803@gmail.com>
2025-07-10 20:00:07 +0530

    master - remove unwanted files

--- SERVER/model/agent_state.py
+++
@@ -1,6 +0,0 @@
-from typing import List, TypedDict, Annotated
-from langgraph.graph import add_messages
-from langchain_core.messages import BaseMessage
-
-class AgentState(TypedDict):
-    messages: Annotated[List[BaseMessage], add_messages]
